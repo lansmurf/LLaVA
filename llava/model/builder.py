60d7104f73ad3230cdbc160a9e8ab151d0cc628c
lansmurf <nejoniaux@gmail.com>
2024-04-23 01:34:57 +0200

    conversation fix llama3

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -123,7 +123,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
     elif 'pretrain' in model_name:
         print('loading adapter')
         tokenizer = AutoTokenizer.from_pretrained(model_base, use_fast=False)
-        cfg_pretrained = LlavaLlamaConfig.from_pretrained(model_path)
+        cfg_pretrained = LlavaConfig.from_pretrained(model_path)
         model = LlavaLlamaForCausalLM.from_pretrained(model_base, low_cpu_mem_usage=True, config=cfg_pretrained,
                                                         **kwargs)
         mm_projector_weights = torch.load(os.path.join(model_path, 'mm_projector.bin'), map_location='cpu')
