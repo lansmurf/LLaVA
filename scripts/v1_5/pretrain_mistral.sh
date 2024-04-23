2cbc2a81f7f01f3b1379168ee4b44f71df3b3f86
lansmurf <nejoniaux@gmail.com>
2024-04-23 20:34:00 +0200

    test mistral

--- scripts/v1_5/pretrain_mistral.sh
+++ scripts/v1_5/pretrain_mistral.sh
@@ -2,7 +2,7 @@
 
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
-    --model_name_or_path mistral-community/Mistral-7B-v0.2 \
+    --model_name_or_path MaziyarPanahi/Mistral-7B-Instruct-v0.2 \
     --version mistral_instruct \
     --data_path /home/nicolas.joniaux/Desktop/llava/chat.json \
     --image_folder /home/nicolas.joniaux/Desktop/llava \
