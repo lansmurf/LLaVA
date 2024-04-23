42237d93ab376bfee49f851d933f07345565b75f
lansmurf <nejoniaux@gmail.com>
2024-04-23 22:01:07 +0200

    test mistralqweqwe

--- llava/train/train.py
+++ llava/train/train.py
@@ -529,7 +529,7 @@ def preprocess_llama_3(
 
         target[cur_len:] = IGNORE_INDEX
 
-    print(f"-------------------------------------\n"
+    '''print(f"-------------------------------------\n"
           f"INPUT_IDS_SHAPE {input_ids.shape}\n"
           f"TARGETS SHAPE {targets.shape}\n"
           f"INPUT IDS ARE {input_ids}\n"
@@ -541,7 +541,7 @@ def preprocess_llama_3(
           f"ROUND LEN IS {round_len}\n"
           f"CUR LEN IS {cur_len}\n"
           f"TOTAL LEN IS {total_len}"
-          f"-------------------------------------\n")
+          f"-------------------------------------\n")'''
 
     return dict(
         input_ids=input_ids,
