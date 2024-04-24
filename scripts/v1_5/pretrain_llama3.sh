13dd5ad3743072d16b9f8124dc791b6ae912b860
lansmurf <nejoniaux@gmail.com>
2024-04-24 15:39:30 +0200

    ready? idk!

--- scripts/v1_5/pretrain_llama3.sh
+++ scripts/v1_5/pretrain_llama3.sh
@@ -17,7 +17,7 @@ deepspeed llava/train/train_mem.py \
     --num_train_epochs 1 \
     --per_device_train_batch_size 12 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 5 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 100 \
