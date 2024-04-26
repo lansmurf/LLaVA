301d2167e332fe3737d3b559337662db5818c0a0
lansmurf <nejoniaux@gmail.com>
2024-04-26 12:21:07 +0200

    routing check

--- llava/serve/inference.py
+++ llava/serve/inference.py
@@ -110,7 +110,7 @@ class ProjectionModule(nn.Module):
 
 def load_projection_module(mm_hidden_size=1152, hidden_size=4096, device="cuda"):
     projection_module = ProjectionModule(mm_hidden_size, hidden_size)
-    checkpoint = torch.load("./mm_projector.bin")
+    checkpoint = torch.load("./checkpoints/llama-3/checkpoint-2300/mm_projector.bin")
     checkpoint = {k.replace("mm_projector.", ""): v for k, v in checkpoint.items()}
     projection_module.load_state_dict(checkpoint)
     projection_module = projection_module.to(device).half()
