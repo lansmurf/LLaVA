64a66c668f3bfe65a792a6f1eac23f164b90856f
lansmurf <nejoniaux@gmail.com>
2024-05-02 22:43:14 +0200

    evalset

--- scripts/v1_5/finetune_llama3.sh
+++ scripts/v1_5/finetune_llama3.sh
@@ -17,7 +17,7 @@ deepspeed llava/train/train_mem.py \
     --bf16 True \
     --output_dir ./checkpoints/llama-3-finetune \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 12 \
+    --per_device_train_batch_size 10 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 5 \
     --evaluation_strategy "no" \
