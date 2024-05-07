40c6ffdf423f7ebcad8c1a6d5013da3985c6082d
lansmurf <nejoniaux@gmail.com>
2024-05-07 23:26:15 +0200

    evalset

--- llava/model/multimodal_encoder/siglip_encoder.py
+++ llava/model/multimodal_encoder/siglip_encoder.py
@@ -115,7 +115,7 @@ class SiglipVisionTowerS2(SiglipVisionTower):
         self.vision_tower.requires_grad_(False)
 
         self.image_processor.size['shortest_edge'] = self.s2_image_size
-        self.image_processor.crop_size['height'] = self.image_processor.crop_size['width'] = self.s2_image_size
+        self.image_processor.size['height'] = self.image_processor.size['width'] = self.s2_image_size
 
         self.is_loaded = True
 
