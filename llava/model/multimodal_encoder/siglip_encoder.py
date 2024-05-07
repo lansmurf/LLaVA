f141af5bcce2f6b7d3228161862674cf6bf022cc
lansmurf <nejoniaux@gmail.com>
2024-05-07 23:49:01 +0200

    evalset

--- llava/model/multimodal_encoder/siglip_encoder.py
+++ llava/model/multimodal_encoder/siglip_encoder.py
@@ -35,7 +35,7 @@ class SiglipVisionTower(nn.Module):
     def feature_select(self, image_forward_outs):
         image_features = image_forward_outs.hidden_states[self.select_layer]
         if self.select_feature == 'patch':
-            image_features = image_features[:, 1:]
+            image_features = image_features[:, :]
         elif self.select_feature == 'cls_patch':
             image_features = image_features
         else:
